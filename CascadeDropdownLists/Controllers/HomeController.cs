9774e0f220fda9ffd87114192a9fe55cd63b94fd
ead2015 <ead.shuja@gmail.com>
2015-11-20 19:34:25 +0500

    Commit-1

---
+++ CascadeDropdownLists/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CascadeDropdownLists.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetCities(int id)
+        {
+            List<Models.City> cities = new List<Models.City>();
+
+            if (id == 1)
+            {
+                cities.Add(new Models.City(1, "Lahore"));
+                cities.Add(new Models.City(2, "Islamabad"));
+            }
+            else {
+                cities.Add(new Models.City(1, "Paris"));
+            }
+            return this.Json(cities,JsonRequestBehavior.AllowGet);
+        }
+             
+    }
+}
---
+++ CascadeDropdownLists/Models/City.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CascadeDropdownLists.Models
+{
+    public class City
+    {
+
+        public City()
+        { }
+
+        public City(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
